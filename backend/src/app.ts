806dcea2c5582b609a6ba1ec0da27e01751d13c4
Mathew Hany <mathew.hanybb@gmail.com>
2023-10-03 21:45:02 +0300

    Add example routes & controllers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,11 @@
-console.log("yarab yshtghl")
+import express, { Express, Request, Response } from "express";
+import { router } from "./routes";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ backend/src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+import { login, register } from "./app/controllers/auth";
+
+export const router = Router();
+
+router.get("/login", login);
+router.get("/register", register);
