a29ce7ec62fb70d0a57392610658253b0172d445
NairuzY <84462600+NairuzY@users.noreply.github.com>
2023-10-05 13:48:16 +0300

    created db model for the patient

---
+++ backend/src/app/models/patientModel.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const patientSchema = new Schema(
+  {
+    Username: { type: String, required: true },
+    Password: { type: String, required: true },
+    Name: { type: String, required: true },
+    Email: { type: String, required: true },
+    MobileNumber: { type: String, required: true },
+    DateOfBirth: { type: Date, required: true },
+    Gender: { type: String, required: true },
+    EmergencyContact: {
+      Name: { type: String, required: true },
+      MobileNumber: { type: String, required: true },
+    },
+  },
+  { timestamps: true }
+);
+
+const Patient = mongoose.model("Patient", patientSchema);
+module.exports = Patient;
