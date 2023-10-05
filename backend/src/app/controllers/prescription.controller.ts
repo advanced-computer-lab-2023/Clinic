b0ce95a89788ba1ef9eb0b30032423261998b7e2
MarwaAwd <marwaawd5@gmail.com>
2023-10-05 23:12:41 +0300

    view and add prescrpictions

---
+++ backend/src/app/controllers/prescription.controller.ts
@@ -0,0 +1,35 @@
+import { PrescriptionModel } from '../models/prescription.model'
+import { Router } from 'express'
+import { asyncWrapper } from '../utils/asyncWrapper'
+import { validate } from '../middlewares/validation.middleware'
+import {
+  type CreatePrescriptionRequest,
+  CreatePrescriptionRequestValidator,
+} from '../types/prescription.types'
+
+export const prescriptionsRouter = Router()
+
+prescriptionsRouter.get(
+  '/',
+  asyncWrapper(async (req, res) => {
+    const prescriptions = await PrescriptionModel.find({})
+
+    res.status(200).json(prescriptions)
+  })
+)
+
+prescriptionsRouter.post(
+  '/',
+  validate(CreatePrescriptionRequestValidator),
+  asyncWrapper<CreatePrescriptionRequest>(async (req, res) => {
+    const { doctor, patient, date } = req.body
+
+    const prescription = await PrescriptionModel.create({
+      doctor,
+      patient,
+      date,
+    })
+
+    res.status(200).json(prescription)
+  })
+)
---
+++ backend/src/app/types/prescription.types.ts
@@ -0,0 +1,22 @@
+import * as zod from 'zod'
+import type { z } from 'zod'
+
+export const CreatePrescriptionRequestValidator = zod.object({
+  date: zod.date(),
+  doctor: zod.string().min(1),
+  patient: zod.string().min(1),
+})
+
+export type CreatePrescriptionRequest = z.infer<typeof CreatePrescriptionRequestValidator>
+
+export class CreatePrescriptionResponse {
+  date: Date
+  doctor: string
+  patient: string
+
+  constructor(date: Date, doctor: string, patient: string) {
+    this.date = date
+    this.doctor = doctor
+    this.patient = patient
+  }
+}
