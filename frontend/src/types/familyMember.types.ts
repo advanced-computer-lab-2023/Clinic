cef6d66884cb0ec82c39405acba91142f7733e96
Mathew Hany <mathew.hanybb@gmail.com>
2023-10-08 00:14:02 +0300

    FE Add Family Member

--- frontend/src/types/familyMember.types.ts
+++ frontend/src/types/familyMember.types.ts
@@ -1,3 +1,6 @@
+import { type z } from 'zod'
+import { type AddFamilyMemberRequestValidator } from '../validators/familyMembers.validator'
+
 export class FamilyMemberResponseBase {
   constructor(
     public id: string,
@@ -5,16 +8,20 @@ export class FamilyMemberResponseBase {
     public nationalId: string,
     public age: number,
     public gender: string,
-    public relation: string
-  ) /**
-   * Removed this because when we request a family member, we already request the
-   * family member by its patient username, so we already know who the patient is.
-   * Leaving this complicates the service code so no need to add it.
-   */
-  // public relatedTo: string
-  {}
+    public relation: string /**
+     * Removed this because when we request a family member, we already request the
+     * family member by its patient username, so we already know who the patient is.
+     * Leaving this complicates the service code so no need to add it.
+     */ // public relatedTo: string
+  ) {}
 }
 
 export class GetFamilyMembersResponse {
   constructor(public familyMembers: FamilyMemberResponseBase[]) {}
 }
+
+export class AddFamilyMemberResponse extends FamilyMemberResponseBase {}
+
+export type AddFamilyMemberRequest = z.infer<
+  typeof AddFamilyMemberRequestValidator
+>
