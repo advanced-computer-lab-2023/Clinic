1cf8920c819afea72465a7daa94dc05f5f8946c9
Mathew Hany <mathew.hanybb@gmail.com>
2023-10-07 23:01:08 +0300

    Update FE types & validations to match BE

--- frontend/src/types/familyMember.types.ts
+++ frontend/src/types/familyMember.types.ts
@@ -1,12 +1,3 @@
-export interface FamilyMember {
-  name: string
-  nationalId: string
-  age: number
-  gender: string
-  relation: string
-  relatedTo: string
-}
-
 export class FamilyMemberResponseBase {
   constructor(
     public id: string,
@@ -14,11 +5,16 @@ export class FamilyMemberResponseBase {
     public nationalId: string,
     public age: number,
     public gender: string,
-    public relation: string,
-    public relatedTo: string
-  ) {}
+    public relation: string
+  ) /**
+   * Removed this because when we request a family member, we already request the
+   * family member by its patient username, so we already know who the patient is.
+   * Leaving this complicates the service code so no need to add it.
+   */
+  // public relatedTo: string
+  {}
 }
 
 export class GetFamilyMembersResponse {
-  constructor(public doctors: FamilyMemberResponseBase[]) {}
+  constructor(public familyMembers: FamilyMemberResponseBase[]) {}
 }
