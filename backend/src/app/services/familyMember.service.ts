9f650d661d8fb09b954302862f6c49d06b56842f
Mathew Hany <mathew.hanybb@gmail.com>
2023-10-07 23:35:47 +0300

    BE Add Family Member

--- backend/src/app/services/familyMember.service.ts
+++ backend/src/app/services/familyMember.service.ts
@@ -1,8 +1,12 @@
 import type { HydratedDocument } from 'mongoose'
-import { type FamilyMemberDocument } from '../models/familyMember.model'
+import {
+  FamilyMemberModel,
+  type FamilyMemberDocument,
+} from '../models/familyMember.model'
 import { UserModel } from '../models/user.model'
 import { NotFoundError } from '../errors'
 import { PatientModel } from '../models/patient.model'
+import { type AddFamilyMemberRequest } from '../types/familyMember.types'
 
 export async function getFamilyMembers(
   username: string
@@ -24,4 +28,29 @@ export async function getFamilyMembers(
   }
 
   return patient.familyMembers
+}
+
+export async function createFamilyMember(
+  patientUsername: string,
+  req: AddFamilyMemberRequest
+): Promise<HydratedDocument<FamilyMemberDocument>> {
+  const user = await UserModel.findOne({ username: patientUsername })
+
+  if (user == null) {
+    throw new NotFoundError()
+  }
+
+  const patient = await PatientModel.findOne({ user: user.id })
+
+  if (patient == null) {
+    throw new NotFoundError()
+  }
+
+  const familyMember = await FamilyMemberModel.create(req)
+
+  patient.familyMembers.push(familyMember.id)
+
+  await patient.save()
+
+  return familyMember
 }
