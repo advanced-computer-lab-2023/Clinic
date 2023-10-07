df3a38b19e92924ff5d6bfb7cdbeb63eeb03ecdf
SaraAlajmy <saraalagami@gmail.com>
2023-10-08 01:35:29 +0300

    conflicts resolved

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,7 +6,6 @@ import { debugRouter } from './app/controllers/debug.controller'
 import { prescriptionsRouter } from './app/controllers/prescription.controller'
 import { familyMemberRouter } from './app/controllers/familyMember.controller'
 import { patientRouter } from './app/controllers/patient.controller'
-import { allowAuthenticated } from './app/middlewares/auth.middleware'
 import { appointmentsRouter } from './app/controllers/appointment.controller'
 import {
   allowAdmins,
@@ -15,7 +14,6 @@ import {
 
 import { adminRouter } from './app/controllers/admin.controller'
 import { asyncWrapper } from './app/utils/asyncWrapper'
-
 
 export const router = Router()
 
@@ -28,7 +26,4 @@ router.use('/prescriptions', prescriptionsRouter)
 router.use('/familyMembers', familyMemberRouter)
 router.use('/patients', patientRouter)
 router.use('/appointment', appointmentsRouter)
-router.use('/admins',
-    asyncWrapper(allowAdmins),
-    adminRouter)
-
+router.use('/admins', asyncWrapper(allowAdmins), adminRouter)
