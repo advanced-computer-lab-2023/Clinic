364337911106299f37a28733dbe1e2d817335c37
Nairuz yasser <84462600+NairuzY@users.noreply.github.com>
2023-10-09 01:38:28 +0300

    added a service to get patient by id and the endpoint that calls it +… (#37)

--- backend/src/app/services/patient.service.ts
+++ backend/src/app/services/patient.service.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from '../errors'
 import { type PatientDocument, PatientModel } from '../models/patient.model'
 import { type UserDocument } from '../models/user.model'
 import { type WithUser } from '../utils/typeUtils'
@@ -18,4 +19,14 @@ export async function getPatientByName(
     .exec()
 
   return patients
+}
+
+export async function getPatientByID(
+  id: string
+): Promise<PatientDocumentWithUser> {
+  const patient = await PatientModel.findOne({ _id: id })
+    .populate<{ user: UserDocument }>('user')
+    .exec()
+  if (patient == null) throw new NotFoundError()
+  return patient
 }
