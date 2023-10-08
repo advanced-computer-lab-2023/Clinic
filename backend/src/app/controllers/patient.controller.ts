837fdf1af62ad5e8dd51d711eb8cebc9d38a8be4
NairuzY <84462600+NairuzY@users.noreply.github.com>
2023-10-09 00:51:09 +0300

    added a service to get patient by id and the endpoint that calls it + added authorization to search and view endpoints
    this endpoint will be called when the user clicks on a certain patient from a list of patients to view his info

--- backend/src/app/controllers/patient.controller.ts
+++ backend/src/app/controllers/patient.controller.ts
@@ -1,17 +1,57 @@
 import { Router } from 'express'
 
 import { asyncWrapper } from '../utils/asyncWrapper'
-import { getPatientByName } from '../services/patient.service'
-import { GetPatientResponse } from 'clinic-common/types/patient.types'
-import { NotAuthenticatedError } from '../errors/auth.errors'
+import { getPatientByID, getPatientByName } from '../services/patient.service'
+import {
+  GetPatientResponse,
+  PatientResponseBase,
+} from 'clinic-common/types/patient.types'
+import {
+  NotAuthenticatedError,
+  NotAuthorizedError,
+} from '../errors/auth.errors'
+import { isDoctorAndApproved } from '../services/auth.service'
 
 export const patientRouter = Router()
+
+patientRouter.get(
+  '/view/:id',
+  asyncWrapper(async (req, res) => {
+    if (req.username == null) {
+      throw new NotAuthenticatedError()
+    }
+    if (!(await isDoctorAndApproved(req.username))) {
+      throw new NotAuthorizedError()
+    }
+    const id = req.params.id
+
+    const patient = await getPatientByID(id)
+    res.send(
+      new PatientResponseBase(
+        patient.id,
+        patient.user.username,
+        patient.name,
+        patient.email,
+        patient.mobileNumber,
+        patient.dateOfBirth,
+        patient.gender,
+        {
+          name: patient.emergencyContact?.name ?? '',
+          mobileNumber: patient.emergencyContact?.mobileNumber ?? '',
+        }
+      )
+    )
+  })
+)
 
 patientRouter.get(
   '/search',
   asyncWrapper(async (req, res) => {
     if (req.username == null) {
       throw new NotAuthenticatedError()
+    }
+    if (!(await isDoctorAndApproved(req.username))) {
+      throw new NotAuthorizedError()
     }
     const name = req.query.name as string
 
