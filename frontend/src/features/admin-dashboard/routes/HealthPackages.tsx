c210d4ff95b9657a5d5df1e3fa447b8518e35b02
MarwaAwd <marwaawd5@gmail.com>
2023-10-10 19:54:50 +0300

    get-health-packages-fe

--- frontend/src/features/admin-dashboard/routes/HealthPackages.tsx
+++ frontend/src/features/admin-dashboard/routes/HealthPackages.tsx
@@ -1,5 +1,14 @@
 import { getHealthPackages } from '@/api/healthPackages'
 import { CardPlaceholder } from '@/components/CardPlaceholder'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Stack,
+  Typography,
+} from '@mui/material'
 
 import { useQuery } from '@tanstack/react-query'
 
@@ -14,4 +23,74 @@ export function HealthPackages() {
   if (query.isError) {
     return <h1>error</h1>
   }
+
+  return (
+    <Grid container spacing={1}>
+      {query.data.healthPackages.map((healthPackage) => {
+        return (
+          <Grid item xl={12}>
+            <Card variant="outlined">
+              <CardContent>
+                <Stack spacing={2}>
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    justifyContent="space-between"
+                    alignItems="center"
+                  >
+                    <Typography variant="h6" color="text.secondary">
+                      {healthPackage.name}
+                    </Typography>
+                  </Stack>
+                  <Stack spacing={-1}>
+                    <Typography variant="overline" color="text.secondary">
+                      Package ID
+                    </Typography>
+                    <Typography variant="body1">{healthPackage.id}</Typography>
+                  </Stack>
+
+                  <Stack spacing={-1}>
+                    <Typography variant="overline" color="text.secondary">
+                      Price per year
+                    </Typography>
+                    <Typography variant="body1">
+                      {healthPackage.pricePerYear}
+                    </Typography>
+                  </Stack>
+                  <Stack spacing={-1}>
+                    <Typography variant="overline" color="text.secondary">
+                      Session Discount
+                    </Typography>
+                    <Typography variant="body1">
+                      {healthPackage.sessionDiscount}
+                    </Typography>
+                  </Stack>
+                  <Stack spacing={-1}>
+                    <Typography variant="overline" color="text.secondary">
+                      Medicine From Our Pharmacy Discount
+                    </Typography>
+                    <Typography variant="body1">
+                      {healthPackage.medicineDiscount}
+                    </Typography>
+                  </Stack>
+                  <Stack spacing={-1}>
+                    <Typography variant="overline" color="text.secondary">
+                      Family Member Subscription Discount
+                    </Typography>
+                    <Typography variant="body1">
+                      {healthPackage.familyMemberSubscribtionDiscount}
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Update</Button>
+                <Button size="small">Delete</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        )
+      })}
+    </Grid>
+  )
 }
