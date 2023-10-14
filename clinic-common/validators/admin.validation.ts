9a9abcbab32c6f3f55f91db3f1427cac30ef3acd
ramezlahzy <rameznashaat9999@gmail.com>
2023-10-14 16:50:09 +0300

    fix username validation error with admin

--- clinic-common/validators/admin.validation.ts
+++ clinic-common/validators/admin.validation.ts
@@ -1,7 +1,7 @@
 import * as zod from 'zod'
 
 export const AddAdminValidator = zod.object({
-  username: zod.string().min(3).max(255),
+  username: zod.string().min(3).max(255).regex(/^[a-zA-Z0-9_]+$/),
   password: zod.string().min(6).max(255),
 })
 
