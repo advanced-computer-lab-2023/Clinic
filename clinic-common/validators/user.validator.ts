db010927fc91b8090750778bbdc5adb339dd0254
ramezlahzy <95905915+ramezlahzy@users.noreply.github.com>
2023-10-14 16:31:32 +0300

    fix username validation error (#71)

--- clinic-common/validators/user.validator.ts
+++ clinic-common/validators/user.validator.ts
@@ -1,7 +1,7 @@
 import * as zod from 'zod'
 
 export const RegisterRequestValidator = zod.object({
-  username: zod.string().min(3).max(255),
+  username: zod.string().min(3).max(255).regex(/^[a-zA-Z0-9_]+$/),
   password: zod.string().min(6).max(255),
   name: zod.string().min(3).max(255),
   email: zod.string().email(),
