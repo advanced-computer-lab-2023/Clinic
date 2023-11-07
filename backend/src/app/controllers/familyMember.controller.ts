1d035f31c82ba4e4904c5f0e18b1eeddef102356
SaraAlajmy <saraalagami@gmail.com>
2023-11-07 14:39:55 +0300

    fixed backend

--- backend/src/app/controllers/familyMember.controller.ts
+++ backend/src/app/controllers/familyMember.controller.ts
@@ -16,9 +16,13 @@ import {
   type Relation,
   GetFamilyMemberResponse,
   FamilyMemberResponseBase,
+  type LinkFamilyMemberRequest,
 } from 'clinic-common/types/familyMember.types'
 import { allowAuthenticated } from '../middlewares/auth.middleware'
-import { AddFamilyMemberRequestValidator } from 'clinic-common/validators/familyMembers.validator'
+import {
+  AddFamilyMemberRequestValidator,
+  LinkFamilyMemberRequestValidator,
+} from 'clinic-common/validators/familyMembers.validator'
 import { validate } from '../middlewares/validation.middleware'
 import { type Gender } from 'clinic-common/types/gender.types'
 import { PatientResponseBase } from 'clinic-common/types/patient.types'
@@ -30,11 +34,6 @@ export const familyMemberRouter = Router()
 
 // Get all family members of the currently logged in patient
 familyMemberRouter.get(
-  /**
-   * Renamed from '/view' to '/mine', makes more sense to say `/family-member/mine`
-   * to get my family members than `/family-member/view` which might be confused
-   * with gettings all family members, not just mine.
-   */
   '/mine',
   allowAuthenticated,
   asyncWrapper(async (req, res) => {
@@ -54,39 +53,47 @@ familyMemberRouter.get(
     )
   })
 )
-familyMemberRouter.post(
-  '/link-family-member',
 
-asyncWrapper(async (req:any, res:any) => {
-  let familyMember= null;
-  if(req.body.email!=null)
-    { const familyMemberEmail=  req.body.email;
-       familyMember= await findFamilyMemberByEmail(familyMemberEmail);
+familyMemberRouter.post(
+  '/link',
+  validate(LinkFamilyMemberRequestValidator),
+  asyncWrapper<LinkFamilyMemberRequest>(async (req: any, res: any) => {
+    let familyMember = null
+    if (req.body.email != null) {
+      const familyMemberEmail = req.body.email
+      familyMember = await findFamilyMemberByEmail(familyMemberEmail)
+    } else if (req.body.mobileNumber != null) {
+      const familyMemberMobileNumber = req.body.mobileNumber
+      familyMember = await findFamilyMemberByMobileNumber(
+        familyMemberMobileNumber
+      )
     }
-    else if(req.body.mobileNumber!=null)
-    { const familyMemberMobileNumber=  req.body.mobileNumber;
-       familyMember= await findFamilyMemberByMobileNumber(familyMemberMobileNumber);
-    }
+    // else{
+    // TODO: throw error
+    // }
+    const calculatedAge =
+      familyMember?.dateOfBirth != null
+        ? new Date().getFullYear() - familyMember.dateOfBirth!.getFullYear()
+        : undefined
 
     const newFamilyMember = new FamilyMemberModel({
-  patient :familyMember?._id,
-  relation:req.body.relation,
-  name:familyMember?.name,
-  nationalId:"0123456789",
-  age:0,
-  gender:familyMember?.gender
+      name: familyMember?.name,
+      nationalId: 'N/A',
+      age: calculatedAge,
+      gender: familyMember?.gender,
+      relation: req.body.relation,
+      patient: familyMember?._id,
     })
-    await newFamilyMember.save();
-    const user = await UserModel.findOne({ username: req.username });
-  const currentUser = await PatientModel.findOne({ user: user?._id });
-  console.log(currentUser)
-if(currentUser==null) 
-return res.status(404).json({ error: 'Current user not found' });
-    currentUser.familyMembers.push(newFamilyMember._id);
+    await newFamilyMember.save()
+    const user = await UserModel.findOne({ username: req.username })
+    const currentUser = await PatientModel.findOne({ user: user?._id })
+    if (currentUser == null)
+      return res.status(404).json({ error: 'Current user not found' })
+    currentUser.familyMembers.push(newFamilyMember._id)
 
-    await currentUser.save();
+    await currentUser.save()
 
-  res.send(familyMember)
+    res.send(familyMember)
   })
 )
 // Create a family member for the patient with the given username
@@ -147,4 +154,3 @@ familyMemberRouter.get(
     )
   })
 )
-
--- frontend/src/features/patient-dashboard/routes/AddFamilyMember.tsx
+++ frontend/src/features/patient-dashboard/routes/AddFamilyMember.tsx
@@ -1,61 +1,75 @@
 import { useAuth } from '@/hooks/auth'
-import { AddFamilyMemberRequest } from 'clinic-common/types/familyMember.types'
-import { AddFamilyMemberRequestValidator } from 'clinic-common/validators/familyMembers.validator'
-import { addFamilyMember } from '@/api/familyMembers'
+import { AddFamilyMemberRequest, LinkFamilyMemberRequest } from 'clinic-common/types/familyMember.types'
+import { AddFamilyMemberRequestValidator, LinkFamilyMemberRequestValidator } from 'clinic-common/validators/familyMembers.validator'
+import { addFamilyMember, linkFamilyMember } from '@/api/familyMembers'
 import { ApiForm } from '@/components/ApiForm'
+import { useState } from 'react'
+import { LinkFamilyMember } from './LinkFamilyMember'
 
 export function AddFamilyMember({ onSuccess }: { onSuccess: () => void }) {
+  const [isLinkingExistingAccount, setIsLinkingExistingAccount] =
+    useState(false)
   const { user } = useAuth()
-
+  const handleLinkExistingAccount = () => {
+    setIsLinkingExistingAccount(true)
+  }
   return (
-    <ApiForm<AddFamilyMemberRequest>
-      fields={[
-        { label: 'Name', property: 'name' },
-        { label: 'National ID', property: 'nationalId' },
-        { label: 'Age', property: 'age', valueAsNumber: true },
-        {
-          label: 'Gender',
-          property: 'gender',
-          selectedValues: [
-            // TODO: use enum
-            {
-              label: 'Male',
-              value: 'male',
-            },
-            {
-              label: 'Female',
-              value: 'female',
-            },
-          ],
-        },
-        {
-          label: 'Relation',
-          property: 'relation',
-          selectedValues: [
-            // TODO: use enum
-            {
-              label: 'Wife',
-              value: 'wife',
-            },
-            {
-              label: 'Husband',
-              value: 'husband',
-            },
-            {
-              label: 'Son',
-              value: 'son',
-            },
-            {
-              label: 'Daughter',
-              value: 'daughter',
-            },
-          ],
-        },
-      ]}
-      validator={AddFamilyMemberRequestValidator}
-      successMessage="Added family member successfully"
-      action={(data) => addFamilyMember(user!.username, data)}
-      onSuccess={onSuccess}
-    />
+    <div>
+      {(isLinkingExistingAccount==false)&&<ApiForm<AddFamilyMemberRequest>
+        fields={[
+          { label: 'Name', property: 'name' },
+          { label: 'National ID', property: 'nationalId' },
+          { label: 'Age', property: 'age', valueAsNumber: true },
+          {
+            label: 'Gender',
+            property: 'gender',
+            selectedValues: [
+              // TODO: use enum
+              {
+                label: 'Male',
+                value: 'male',
+              },
+              {
+                label: 'Female',
+                value: 'female',
+              },
+            ],
+          },
+          {
+            label: 'Relation',
+            property: 'relation',
+            selectedValues: [
+              // TODO: use enum
+              {
+                label: 'Wife',
+                value: 'wife',
+              },
+              {
+                label: 'Husband',
+                value: 'husband',
+              },
+              {
+                label: 'Son',
+                value: 'son',
+              },
+              {
+                label: 'Daughter',
+                value: 'daughter',
+              },
+            ],
+          },
+        ]}
+        validator={AddFamilyMemberRequestValidator}
+        successMessage="Added family member successfully"
+        action={(data) => addFamilyMember(user!.username, data)}
+        onSuccess={onSuccess}
+      />}
+      {(isLinkingExistingAccount==false)&&<button onClick={handleLinkExistingAccount}>
+        Or link existing patient account
+      </button>}
+      {isLinkingExistingAccount && (
+        LinkFamilyMember({ onSuccess: () => alert("Family member linked successfully") })
+      )}
+    </div>
   )
 }
