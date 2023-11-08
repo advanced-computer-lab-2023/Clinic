0bb19856afc0af6d329edb0bec554799e1a8778d
SaraAlajmy <saraalagami@gmail.com>
2023-11-08 07:03:35 +0300

    add new lines and removed extra debug commands

--- frontend/src/api/familyMembers.ts
+++ frontend/src/api/familyMembers.ts
@@ -9,6 +9,7 @@ import { AddFamilyMemberRequest } from 'clinic-common/types/familyMember.types'
 import { AddFamilyMemberResponse } from 'clinic-common/types/familyMember.types'
 
 export async function getFamilyMembers(): Promise<GetFamilyMembersResponse> {
+  
   return await api
     .get<GetFamilyMembersResponse>(`/family-members/mine`)
     .then((res) => res.data)
@@ -17,6 +18,7 @@ export async function getFamilyMembers(): Promise<GetFamilyMembersResponse> {
 export async function getFamilyMemberById(
   id: string
 ): Promise<GetFamilyMemberResponse> {
+
   return await api
     .get<GetFamilyMemberResponse>(`/family-members/${id}`)
     .then((res) => res.data)
@@ -26,6 +28,7 @@ export async function addFamilyMember(
   username: string,
   req: AddFamilyMemberRequest
 ): Promise<AddFamilyMemberResponse> {
+
   return await api
     .post<AddFamilyMemberResponse>(`/family-members/${username}`, req)
     .then((res) => res.data)
@@ -34,7 +37,6 @@ export async function addFamilyMember(
 export async function linkFamilyMember(
   req: LinkFamilyMemberRequest
 ): Promise<LinkFamilyMemberResponse> {
-  console.log("I'm here");
 
   return await api
     .post<LinkFamilyMemberResponse>(`/family-members/link`, req)
