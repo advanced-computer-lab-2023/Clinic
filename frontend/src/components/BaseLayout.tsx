8c24c1625c50be7ec5a899203f2d9abe1a681d90
NairuzY <84462600+NairuzY@users.noreply.github.com>
2023-12-06 14:54:53 +0200

    added navigation buttons to the base layout

--- frontend/src/components/BaseLayout.tsx
+++ frontend/src/components/BaseLayout.tsx
@@ -17,10 +17,13 @@ import React, { useState } from 'react'
 import { Link, Outlet, useLocation } from 'react-router-dom'
 import { OnlyAuthenticated } from './OnlyAuthenticated'
 import { NotificationsList } from './Notifications'
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import { ChatsList } from './chats/ChatsList'
 import { ChatsProvider } from '@/providers/ChatsProvider'
 import { useAuth } from '@/hooks/auth'
+
+import { useNavigate } from 'react-router-dom'
 
 interface ListItemLinkProps {
   icon?: React.ReactElement
@@ -72,6 +75,18 @@ export function BaseLayout() {
     setOpenDrawer(false)
   }
 
+  const navigate = useNavigate()
+
+  // Function to handle the back button click
+  const handleBackButtonClick = () => {
+    navigate(-1) // This will navigate back one step in the browser history
+  }
+
+  // Function to handle the forward button click
+  const handleForwardButtonClick = () => {
+    navigate(1) // This will navigate forward one step in the browser history
+  }
+
   const layout = (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -89,6 +104,26 @@ export function BaseLayout() {
           >
             <MenuIcon />
           </IconButton>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleBackButtonClick}
+            sx={{ mr: 2 }}
+          >
+            <ArrowBackIcon />
+          </IconButton>
+
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleForwardButtonClick}
+            sx={{ mr: 2 }}
+          >
+            <ArrowForwardIcon />
+          </IconButton>
+
           <Typography variant="h6" noWrap component="div">
             Clinic
           </Typography>
