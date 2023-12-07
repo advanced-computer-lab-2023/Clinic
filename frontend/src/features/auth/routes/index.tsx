8ad3fb61392e1ffed103da38febc50199d99ef84
Nairuz yasser <84462600+NairuzY@users.noreply.github.com>
2023-12-07 11:05:05 +0200

    Feature/enhance UI/ux (#198)
    * added some data to the doctor dashboard
    
    * changed the sign in page
    
    * Update package-lock.json

--- frontend/src/features/auth/routes/index.tsx
+++ frontend/src/features/auth/routes/index.tsx
@@ -1,10 +1,11 @@
 import { RouteObject } from 'react-router-dom'
-import { Login } from './Login'
+
 // import { Register } from './Register'
 import { AuthLayout } from '../components/AuthLayout'
 import { Logout } from './Logout'
 // import { RequestDoctor } from '@/features/auth/routes/RequestDoctor'
 import Signup from './signup'
+import SignIn from './SignIn'
 
 export const authRoutes: RouteObject[] = [
   {
@@ -12,7 +13,7 @@ export const authRoutes: RouteObject[] = [
     children: [
       {
         path: 'login',
-        element: <Login />,
+        element: <SignIn />,
       },
       // {
       //   path: 'register',
