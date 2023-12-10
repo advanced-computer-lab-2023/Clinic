579a5fa8687e5d39294daa6909a5b01ba4292e6e
MarwaAwd <marwaawd5@gmail.com>
2023-12-10 16:25:20 +0200

    fix employment contract

--- frontend/src/features/doctor-dashboard/routes/EmploymentContract.tsx
+++ frontend/src/features/doctor-dashboard/routes/EmploymentContract.tsx
@@ -5,14 +5,7 @@ import {
 } from '@/api/doctor'
 import { CardPlaceholder } from '@/components/CardPlaceholder'
 import { useAuth } from '@/hooks/auth'
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Stack,
-  Typography,
-} from '@mui/material'
+import { Button, CardActions, List, ListItem, Typography } from '@mui/material'
 import { useQuery } from '@tanstack/react-query'
 import { ContractStatus } from 'clinic-common/types/doctor.types'
 import { useNavigate } from 'react-router-dom'
@@ -65,7 +58,40 @@ export function EmploymentContract() {
   return (
     <>
       <ToastContainer />
-      <Card variant="outlined">
+
+      <Title>Employment Contract</Title>
+      <Typography variant="body1">
+        This contract is made and entered into between:
+      </Typography>
+      <List>
+        <ListItem>
+          <strong>Clinic, </strong> El7a2ni.
+        </ListItem>
+        <ListItem>
+          <strong>{query.data?.name}, </strong> a licensed physician with a
+          practice at El7a2ni clinic in {query.data?.speciality} departement.
+        </ListItem>
+      </List>
+      <Typography variant="body1">
+        The term of this contract shall be for a period of{' '}
+        <strong>{query.data?.employmentContract.at(4)?.slice(17)}</strong>
+      </Typography>
+      <List>
+        <ListItem>
+          <strong>Doctor fees per hour:</strong> {query.data?.hourlyRate} E£
+        </ListItem>
+        <ListItem>
+          <strong>Clinic Markup : </strong>{' '}
+          {query.data?.employmentContract.at(2)?.slice(13)}
+        </ListItem>
+      </List>
+      <Typography variant="body1">
+        This contract constitutes the entire agreement between the parties and
+        supersedes all prior or contemporaneous communications, representations,
+        or agreements, whether oral or written.
+      </Typography>
+
+      {/* <Card variant="outlined">
         <CardContent>
           <Stack spacing={2}>
             <Stack
@@ -82,33 +108,32 @@ export function EmploymentContract() {
               ))}
             </Stack>
           </Stack>
-        </CardContent>
-        {!isContractAccepted && (
-          <CardActions>
-            <Button
-              size="small"
-              variant="contained"
-              color="success"
-              onClick={() => {
-                handleAccept()
-              }}
-            >
-              accept
-            </Button>
-            <Button
-              size="small"
-              variant="contained"
-              color="error"
-              disabled={query.data?.contractStatus === ContractStatus.Rejected}
-              onClick={() => {
-                handleReject()
-              }}
-            >
-              reject
-            </Button>
-          </CardActions>
-        )}
-      </Card>
+              </CardContent>*/}
+      {!isContractAccepted && (
+        <CardActions>
+          <Button
+            size="small"
+            variant="contained"
+            color="success"
+            onClick={() => {
+              handleAccept()
+            }}
+          >
+            accept
+          </Button>
+          <Button
+            size="small"
+            variant="contained"
+            color="error"
+            disabled={query.data?.contractStatus === ContractStatus.Rejected}
+            onClick={() => {
+              handleReject()
+            }}
+          >
+            reject
+          </Button>
+        </CardActions>
+      )}
     </>
   )
 }
