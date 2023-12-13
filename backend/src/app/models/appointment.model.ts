7fb91e4abd22e8c423bd236eba3f2a0931d9f07d
Farida Abdelghaffar <92890314+FaridaAbdelghaffar@users.noreply.github.com>
2023-12-13 21:29:04 +0200

    fix follow-ups from doctor

--- backend/src/app/models/appointment.model.ts
+++ backend/src/app/models/appointment.model.ts
@@ -15,8 +15,8 @@ const appointmentSchema = new Schema(
     },
     familyID: { type: String, default: '' },
     reservedFor: { type: String, required: true },
-    paidByPatient: { type: Number, required: true },
-    paidToDoctor: { type: Number, required: true },
+    paidByPatient: { type: Number, required: true, default: 0 },
+    paidToDoctor: { type: Number, required: true, default: 0 },
   },
   { timestamps: true }
 )
